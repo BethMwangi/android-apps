ed0585c870cf17a947a9a728de750578e743ba97
BethMwangi <wanjikumwangib@gmail.com>
2016-05-24 23:26:25 +0300

    firstapp

---
+++ FirstApp/app/src/main/java/com/example/beth/firstapp/DiamondActivity.java
@@ -0,0 +1,16 @@
+package com.example.beth.firstapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by beth on 5/24/16.
+ */
+public class DiamondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diamond);
+    }
+}
