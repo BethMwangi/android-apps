273eafc3dbc2223383c35e635f62d71043821432
BethMwangi <wanjikumwangib@gmail.com>
2016-05-25 19:10:42 +0300

    created a listview file

---
+++ ListView/app/src/main/java/com/beth/beth/listview/ListActivity.java
@@ -0,0 +1,16 @@
+package com.beth.beth.listview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by beth on 5/25/16.
+ */
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list);
+    }
+}
