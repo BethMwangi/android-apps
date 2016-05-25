31130724dc2056cf2d0301fcfbaa1b7c2e9fd700
BethMwangi <wanjikumwangib@gmail.com>
2016-05-25 19:58:21 +0300

    list view generated

--- ListView/app/src/main/java/com/beth/beth/listview/ListActivity.java
+++ ListView/app/src/main/java/com/beth/beth/listview/ListActivity.java
@@ -1,7 +1,11 @@
 package com.beth.beth.listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 /**
  * Created by beth on 5/25/16.
@@ -11,6 +15,13 @@ public class ListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list);
+        setContentView(R.layout.activity_main);
+
+
     }
+
 }
+
+
+
+
--- ListView/app/src/main/java/com/beth/beth/listview/MainActivity.java
+++ ListView/app/src/main/java/com/beth/beth/listview/MainActivity.java
@@ -1,13 +1,53 @@
 package com.beth.beth.listview;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+    String[] candidates = {
+            "Jane kimani",
+            "John karani",
+            "Charles Ketwony",
+            "Betty Kirui",
+            "Elizabeth Mwangi",
+            "John Butere",
+            "William Henry",
+            "Brian jackson",
+            "Millard Fillmore",
+            "Brillian Chebet"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+// setContentView(R.layout.list);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, candidates));
     }
-}
+    public void onListItemClick(
+            ListView parent, View v, int position, long id
+    )
+    {
+        Toast.makeText(this, "You have selected" + candidates[position], Toast.LENGTH_LONG).show();
+    }
+    public void addButtonOnClickListener(){ //links to list page
+        Button button;
+
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }}
